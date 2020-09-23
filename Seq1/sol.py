d8d6fe88e5ad3a49c3a22d107ace4d70546619c8
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-23 20:04:10 +0530

    feat: add minimised code

--- Seq1/sol.py
+++ Seq1/sol.py
@@ -12,3 +12,15 @@ T=int(input())
 for _ in range(0,T):
     print(some_operation([int(i) for i in input().split()][:3]))
 
+
+
+# Minimised code
+from math import log
+def s(x):
+    a = 0
+    b=log(2)
+    for n in range(x[0],x[1]):
+        a+=(1 + n*b)/log(n)
+    return round(a,x[2])
+for _ in range(0,int(input())):
+    print(s([int(i) for i in input().split()][:3]))
