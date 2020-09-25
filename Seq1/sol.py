5e4062ee6cb21261e81b769886c17ac74ee42d62
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-09-25 20:19:16 +0530

    Seq1: new challenges, fixes done, yml updated

--- Seq1/sol.py
+++ Seq1/sol.py
@@ -1,10 +1,16 @@
+# Big Code
 
 import math
 def some_operation(arr):
-    ans = 0.0
+    ans = 0
     log2=math.log(2)
-    for n in range(arr[0],arr[1]):
-        ans+=(1 + n*log2)/math.log(n)
+
+
+    for n in range(arr[0],arr[1]+1):
+        i=(1 + n*log2)/math.log(n)
+        ans+=i
+        print(i)
+    print()
     return round(ans,arr[2])
 
 T=int(input())
@@ -13,14 +19,21 @@ for _ in range(0,T):
     print(some_operation([int(i) for i in input().split()][:3]))
 
 
+T=int(input())
 
-# Minimised code
-from math import log
-def s(x):
-    a = 0
-    b=log(2)
-    for n in range(x[0],x[1]):
-        a+=(1 + n*b)/log(n)
-    return round(a,x[2])
-for _ in range(0,int(input())):
-    print(s([int(i) for i in input().split()][:3]))
+
+## Shorter code
+
+import math
+ans=0
+seq=[0 for _ in range(10**5 + 1)]
+log2=math.log(2)
+
+ans=0
+for i in range(2,10**5 +1):
+	ans+=(1+i*log2)/math.log(i)
+	seq[i]=ans
+for _ in range(int(input())):
+    l=[int(i) for i in input().split()][:3]
+    ans=seq[l[1]]-seq[l[0]-1]
+    print('{:.{prec}f}'.format(ans, prec=l[2]))
