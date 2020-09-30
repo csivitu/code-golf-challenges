402ffa3cfd8f78c77a064436aaca7b517a942697
SrishtiGohain <59062633+SrishtiGohain@users.noreply.github.com>
2020-09-30 21:43:35 +0530

    fix: sol.java to take multiple inputs

--- Simple Champagne Tower/sol.java
+++ Simple Champagne Tower/sol.java
@@ -38,11 +38,13 @@ public class SCT
 	} 
 
 	public static void main(String[] args) 
-	{   Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-        int T=sc.nextInt();
-		
-		System.out.println( champagneGlasses(N, T)); 
+    {   Scanner sc=new Scanner(System.in);
+        int k=sc.nextInt();
+        while(k>0)
+        { k--;
+	 int N=sc.nextInt();
+         int T=sc.nextInt();
+         System.out.println( champagneGlasses(N, T)); 
+        }
 	} 
 }
-
