f0d8dc13bcca45debe1c0d9f0732e41cda99f8ec
SrishtiGohain <59062633+SrishtiGohain@users.noreply.github.com>
2020-10-02 16:56:08 +0530

    feat: added challenge Candy Store

---
+++ Candy Store/sol.py
@@ -0,0 +1,19 @@
+def hcf(p, q): 
+	if (q == 0): 
+		return p 
+	return hcf(q, p % q) 
+	
+ 
+def func(S, p, q): 
+	g = hcf(p, q) 
+	if (S % g == 0): 
+		return 1
+	else: 
+		return 0
+		 
+S = int(input())
+p = int(input())
+q = int(input())
+print(func(S,p,q)) 
+
+
