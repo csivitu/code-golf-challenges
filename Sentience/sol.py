01931afd289065fed6f1b32108a18e1bc6241c5b
Arushi Jain <arushijain1479@gmail.com>
2020-10-03 23:13:53 +0530

    fix: Made changes to contraints in challenge Sentience

--- Sentience/sol.py
+++ Sentience/sol.py
@@ -1,5 +1,20 @@
-import math
+mod=pow(10,9)+7
 
-n = int(input())
-n +=1
-print(round( ((1+math.sqrt(5))/2)**n / math.sqrt(5) )) 
+def fibonacci(n): 
+    a = 0
+    b = 1
+    if n == 0: 
+        return a 
+    elif n == 1: 
+        return b 
+    else: 
+        for i in range(2,n+1): 
+            c = (a + b)%mod
+            a = b 
+            b = c 
+        return b 
+  
+t=int(input())
+for _ in range(t):
+	n=int(input())
+	print(fibonacci(n+1)) 
